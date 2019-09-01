8e14a727e16c47803f04e1622dc12db1f80871e5
Taein Kim <kti91@likelion.org>
2019-09-01 21:39:30 +0900

    Customer Component(v1)

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import Customer from "./components/Customer";
+
+const customer = {
+  name: "김태인",
+  birthday: "910422",
+  gender: "남자",
+  job: "취업준비생"
+};
 
 function App() {
   return (
-    <div className="gray-background">
-      <img src={logo} lat="logo" />
-      <h2>Let's develop management system</h2>
-    </div>
+    <Customer
+      name={customer.name}
+      birthday={customer.birthday}
+      gender={customer.gender}
+      job={customer.job}
+    />
   );
 }
 
