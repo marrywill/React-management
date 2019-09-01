55904c239470a773d59182be6843e63ba244293a
Taein Kim <kti91@likelion.org>
2019-09-01 21:58:05 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -3,21 +3,49 @@ import logo from "./logo.svg";
 import "./App.css";
 import Customer from "./components/Customer";
 
-const customer = {
-  name: "김태인",
-  birthday: "910422",
-  gender: "남자",
-  job: "취업준비생"
-};
+const customers = [
+  {
+    id: 1,
+    image: "https://placeimg.com/64/64/1",
+    name: "김태인",
+    birthday: "910422",
+    gender: "남자",
+    job: "취업준비생"
+  },
+  {
+    id: 2,
+    image: "https://placeimg.com/64/64/2",
+    name: "어머니",
+    birthday: "670707",
+    gender: "여자",
+    job: "주부"
+  },
+  {
+    id: 3,
+    image: "https://placeimg.com/64/64/3",
+    name: "최선우",
+    birthday: "910505",
+    gender: "남자",
+    job: "취업준비생"
+  }
+];
 
 function App() {
   return (
-    <Customer
-      name={customer.name}
-      birthday={customer.birthday}
-      gender={customer.gender}
-      job={customer.job}
-    />
+    <div>
+      {customers.map(customer => {
+        return (
+          <Customer
+            key={customer.id}
+            id={customer.id}
+            image={customer.image}
+            name={customer.name}
+            gender={customer.gender}
+            birthday={customer.birthday}
+          />
+        );
+      })}
+    </div>
   );
 }
 
--- src/components/Customer.js
+++ src/components/Customer.js
@@ -4,10 +4,37 @@ class Customer extends React.Component {
   render() {
     return (
       <div>
-        <h2>{this.props.name}</h2>
+        <CustomerProfile id={this.props.id} image={this.props.image} />
+        <CustomerInfo
+          job={this.props.job}
+          birthday={this.props.birthday}
+          gender={this.props.gender}
+        />
+      </div>
+    );
+  }
+}
+
+class CustomerInfo extends React.Component {
+  render() {
+    return (
+      <div>
         <p>{this.props.birthday}</p>
         <p>{this.props.gender}</p>
         <p>{this.props.job}</p>
+      </div>
+    );
+  }
+}
+
+class CustomerProfile extends React.Component {
+  render() {
+    return (
+      <div>
+        <img src={this.props.image} alt="profile" />
+        <h2>
+          {this.props.name}({this.props.id})
+        </h2>
       </div>
     );
   }
